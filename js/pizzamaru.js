ea40bb6051ea88f76003418b7819b9ffb9c23d42
dodo0416 <125528660+dodo0416@users.noreply.github.com>
2023-02-22 05:05:51 +0000

    pizzamaru연습

---
+++ js/pizzamaru.js
@@ -0,0 +1,41 @@
+var swipermainpage = new Swiper(".mainpage", {
+  pagination: {
+    el: ".mainpage .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".mainpage .swiper-button-next",
+    prevEl: ".mainpage .swiper-button-prev",
+  }, spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+var swipermainpage6 = new Swiper(".mainpage6", {
+  navigation: {
+    nextEl: ".mainpage6 .swiper-button-next",
+    prevEl: ".mainpage6 .swiper-button-prev",
+  }, spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".mainpage6 .swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(function () {
+  $(".inner i").click(function () {
+    $("nav").toggleClass("on");
+    $(this).toggleClass("fa-xmark")
+  });
+
+});
